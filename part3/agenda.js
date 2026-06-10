65c95cd2137bd0096ce8c20120e135576c7f2e97
Thom4x <tomhoser.2108@hotmail.com>
2026-06-10 14:56:17 -0500

    part3: 3.4 3.5 HTTP DELETE/POST

--- part3/agenda.js
+++ part3/agenda.js
@@ -4,24 +4,24 @@ app.use(express.json())
 
 let phoneBook = [
     {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456"
     },
     {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
     },
     {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345"
     },
     {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
+        id: 4,
+        name: "Mary Poppendieck",
+        number: "39-23-6423122"
     }
 ]
 
@@ -46,6 +46,44 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
         response.status(404).json({ error: "Missing.." })
     }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const initialLength = phoneBook.length
+
+    phoneBook = phoneBook.filter(usr => usr.id !== id)
+
+    if (phoneBook.length !== initialLength) {
+        response.status(202).end()
+    } else {
+        response.status(404).json({ error: "ID Not found.." })
+    }
+})
+
+const generateId = () => {
+    const random = Math.floor(Math.random() * 1001);
+    return random
+}
+
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const newObject = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    phoneBook = phoneBook.concat(newObject)
+    response.json(newObject)
 })
 
 const PORT = 3001
